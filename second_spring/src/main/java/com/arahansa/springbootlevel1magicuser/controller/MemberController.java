a029dab6c629a45efa06a47c336b1cc89a48d0c9
arahansa <arahansa@naver.com>
2020-07-07 04:59:34 +0900

    [ 확정 ] 페이징

--- second_spring/src/main/java/com/arahansa/springbootlevel1magicuser/controller/MemberController.java
+++ second_spring/src/main/java/com/arahansa/springbootlevel1magicuser/controller/MemberController.java
@@ -4,12 +4,14 @@ import com.arahansa.springbootlevel1magicuser.domain.Member;
 import com.arahansa.springbootlevel1magicuser.service.MemberService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
@@ -23,10 +25,18 @@ public class MemberController {
     @Autowired
     MemberService memberService;
 
+    @GetMapping("/dummy")
+    @ResponseBody
+    public String dummy(){
+        memberService.createDummy();
+        return "OK";
+    }
+
     // /member
     @GetMapping
-    public String index(Model model){
-        model.addAttribute("list", memberService.findAll());
+    public String index(Pageable pageable, Model model){
+        log.info("page : {}", pageable);
+        model.addAttribute("page", memberService.findAll(pageable));
         model.addAttribute("member", new Member());
         return "member";
     }
@@ -41,5 +51,7 @@ public class MemberController {
         memberService.save(member);
         return "redirect:/member";
     }
+
+
 
 }
--- second_spring/src/main/java/com/arahansa/springbootlevel1magicuser/service/MemberService.java
+++ second_spring/src/main/java/com/arahansa/springbootlevel1magicuser/service/MemberService.java
@@ -3,6 +3,8 @@ package com.arahansa.springbootlevel1magicuser.service;
 import com.arahansa.springbootlevel1magicuser.domain.Member;
 import com.arahansa.springbootlevel1magicuser.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,6 +13,19 @@ import java.util.List;
 @Service
 @Transactional
 public class MemberService {
+
+    public void createDummy() {
+        for (int i = 0; i < 200; i++) {
+            Member member = new Member();
+            member.setUsername("user" + i + "@naver.com");
+            member.setPassword("1234");
+            repository.save(member);
+        }
+    }
+
+    public Page<Member> findAll(Pageable pageable) {
+        return repository.findAll(pageable);
+    }
 
     @Autowired
     private MemberRepository repository;
@@ -22,4 +37,6 @@ public class MemberService {
     public Member save(Member member) {
         return repository.save(member);
     }
+
+
 }
