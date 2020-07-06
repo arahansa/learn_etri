56143f0089ddeaaeffdaf53c473f6c14875ed0fb
arahansa <arahansa@naver.com>
2020-07-07 02:33:49 +0900

    second first hello world

---
+++ second_spring/src/test/java/com/arahansa/springbootlevel1magicuser/PlainTest.java
@@ -0,0 +1,12 @@
+package com.arahansa.springbootlevel1magicuser;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PlainTest {
+    @Test
+    public void contextLoads() {
+        assertThat("Hello World").isEqualTo("Hello World");
+    }
+}
---
+++ second_spring/src/test/java/com/arahansa/springbootlevel1magicuser/Springbootlevel1magicuserApplicationTests.java
@@ -0,0 +1,15 @@
+package com.arahansa.springbootlevel1magicuser;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Springbootlevel1magicuserApplicationTests {
+
+	@Test
+	public void contextLoads() {
+
+	}
+
+}
+
