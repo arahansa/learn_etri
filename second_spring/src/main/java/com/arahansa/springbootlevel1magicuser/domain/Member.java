d4cea3956f3e27eed31a3833dd4b6b9bc3fdd161
arahansa <arahansa@naver.com>
2020-07-07 04:43:49 +0900

    멤버 디비 처리 완료

--- second_spring/src/main/java/com/arahansa/springbootlevel1magicuser/domain/Member.java
+++ second_spring/src/main/java/com/arahansa/springbootlevel1magicuser/domain/Member.java
@@ -3,11 +3,20 @@ package com.arahansa.springbootlevel1magicuser.domain;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @Data
+@Entity
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @Length(min = 2)
     @Length(max = 20)
