a0954b34d8cfd310bae937b97bf01573ee6bb014
arahansa <arahansa@naver.com>
2020-07-07 03:06:53 +0900

    [확정 ] 첫번째 테스트 실행

--- second_spring/src/test/java/com/arahansa/springbootlevel1magicuser/PlainTest.java
+++ second_spring/src/test/java/com/arahansa/springbootlevel1magicuser/PlainTest.java
@@ -1,6 +1,6 @@
 package com.arahansa.springbootlevel1magicuser;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
--- second_spring/src/test/java/com/arahansa/springbootlevel1magicuser/Springbootlevel1magicuserApplicationTests.java
+++ second_spring/src/test/java/com/arahansa/springbootlevel1magicuser/Springbootlevel1magicuserApplicationTests.java
@@ -1,15 +1,25 @@
 package com.arahansa.springbootlevel1magicuser;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(
+		classes = Springbootlevel1magicuserApplication.class,
+		properties = {"property.value=propertyTest"}
+		,webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class Springbootlevel1magicuserApplicationTests {
 
+	@Value("${property.value}")
+	private String propertyValue;
+
+
 	@Test
-	public void contextLoads() {
-
+	public void contextLoadsTest() {
+		System.out.println(propertyValue);
+		assertThat(propertyValue).isEqualTo("propertyTest");
 	}
-
 }
 
